f8e198bc2e0ce5d26197cfcce8497032306cab1a
MoonGSK <63923585+Moonbbyte@users.noreply.github.com>
2022-12-22 20:57:09 -0600

    backend1

--- backend/src/routes/usuarios.routes.ts
+++ backend/src/routes/usuarios.routes.ts
@@ -1,13 +1,14 @@
 declare var require: any;
 const {Router} = require('express')
 const fs = require('fs')
+const path = require('path')
 const readline = require('readline')
 const {check} = require('express-validator')
 const validarAtributo= require('../middleware/validarAtributo');
 require('dotenv').config()
 const router = Router()
 const usuariosController = require('../controllers/usuarios.controllers.ts')
-/*
+
 
 router.get('/',(req:any,res:any)=>{
     res.json({holaaa: "hijooo"})
@@ -21,22 +22,29 @@ router.get('/getUsers',(req:any,res:any)=>{
 router.post('/vLog',function(req:any,res:any){
     let usuario:string=req.body.user;
     let password:string=req.body.password;
-    File = fs.open("../BaseDatos/BaseDatos.json")
-    let CBdatos = File.toString()
-    let Bdatos = JSON.stringify(CBdatos);
 
-    
-    if(usuario.includes("@")){
-    }else{
-
+    const pathFile = path.join(__dirname,'../BaseDatos/BaseDatos.json')
+    const texto:string = String(fs.readFileSync(pathFile,'utf-8'))
+    let Bdatos = JSON.parse(texto);
+    let tipoUser:string = "null"
+    let login_correcto:boolean= false
+    try{
+        let usuarios:[] = Bdatos.usuarios;
+        usuarios.forEach((usuario)=>{
+            if(usuario['usuario'] === usuario || usuario['email']==usuario){
+                if(usuario['password']===password){
+                    login_correcto=true
+                    tipoUser=usuario['tipo_usuario']
+                    return;
+                }
+            }
+        })
+    }catch(e){
+        alert("ERROR AL LEER EL ARCHIVO")
     }
+    
+    res.json({"tipo_usuario":tipoUser,"login_correcto":login_correcto})
 })
 
-router.post('/getUser',[
-    check('id', 'El id es obligatorio').not().isEmpty(),
-    validarAtributo
-    ],
-    usuariosController.getUser
-)
-*/
+
 module.exports=router;
